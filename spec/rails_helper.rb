bba6311508afdf820e005aba581dc86e15d63e3b
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 11:01:25 -0600

    temporarily comment out vcr and webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,8 +67,8 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+#   config.hook_into :webmock
+#   config.configure_rspec_metadata!
+# end
