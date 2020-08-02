d2230c45ffb3e0a16b9ab3b129892d10e109056c
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 12:05:55 -0600

    add get_forecast method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,11 +2,18 @@ class ForecastFacade
   attr_reader :weather, :location
   def initialize(location)
     @location = get_location(location)
+    @forecast = get_forecast
   end
 
   def get_location(location)
     map_service = MapService.new
     location_data = map_service.location(location)
-    location = Location.new(location_data)
+    Location.new(location_data)
+  end
+
+  def get_forecast
+    forecast_service = ForecastService.new
+    forecast = forecast_service.forecast(@location.latitude, @location.longitude)
+    Forecast.new(forecast)
   end
 end
