662d4f9925b9d9d316e6ca17bb194fd3e4223ba3
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 16:58:46 -0600

    add forecast_serializer

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastsController < ApplicationController
   def show
     forecast = ForecastFacade.new(forecast_params[:location])
+    render json: ForecastSerializer.new(forecast)
   end
 
   private
