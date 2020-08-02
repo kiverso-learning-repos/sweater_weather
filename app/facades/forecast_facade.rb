e65b19200fd6a1872a9654be3fc859942bc875c2
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 16:59:46 -0600

    add id and timezone offset instance variables

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,14 +1,15 @@
 class ForecastFacade
-  attr_reader :city, :state, :current_weather, :hourly_forecast, :daily_forecast
+  attr_reader :id, :city, :state, :current_weather, :hourly_forecast, :daily_forecast, :timezone_offset
   def initialize(location)
     forecast_location = get_location(location)
     forecast = get_forecast(forecast_location)
+    @id = SecureRandom.uuid
     @city = forecast_location.city
     @state = forecast_location.state
-    @current_weather = format_current_weather(forecast.current)
-    @hourly_forecast = format_hourly_forecast(forecast.hourly)
-    @daily_forecast = format_daily_forecast(forecast.daily)
-    binding.pry
+    @current_weather = forecast.current
+    @hourly_forecast = format_hourly_forecast(forecast)
+    @daily_forecast = format_daily_forecast(forecast)
+    @timezone_offset = forecast.timezone_offset
   end
 
   private
@@ -25,30 +26,24 @@ class ForecastFacade
     Forecast.new(forecast_data)
   end
 
-  def format_current_weather(current)
-    current[:sunrise] = Time.at(current[:sunrise]).strftime('%l:%M %p')
-    current[:sunset] = Time.at(current[:sunset]).strftime('%l:%M %p')
-    current[:visibility] = (current[:visibility] / 1609).to_f
-    current
-  end
-
-  def format_hourly_forecast(hourly)
+  def format_hourly_forecast(forecast)
+    hourly = forecast.hourly
     hourly.map do |hour|
-      {time: Time.at(hour[:dt]).strftime('%l %p'),
+      {time: hour[:dt],
        temp: hour[:temp],
        weather: hour[:weather]
       }
     end
   end
 
-  def format_daily_forecast(daily)
-    daily.map do |day|
+  def format_daily_forecast(forecast)
+    forecast.daily.map do |day|
       if day[:rain]
         precipitation = day[:rain]
       else
         precipitation = 0
       end
-      {day: Time.at(day[:dt]).strftime('%A'),
+      {time: day[:dt],
        temp: day[:temp],
        weather: day[:weather],
        precipitation: precipitation
--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -1,6 +1,7 @@
 class Forecast
-  attr_reader :current, :hourly, :daily
+  attr_reader :timezone_offset, :current, :hourly, :daily
   def initialize(forecast_data)
+    @timezone_offset = forecast_data[:timezone_offset]
     @current = forecast_data[:current]
     @hourly = forecast_data[:hourly]
     @daily = forecast_data[:daily]
