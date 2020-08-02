7d9e410c1d80ccf435f063532a16381b34909972
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 14:14:01 -0600

    add weather service

---
+++ app/services/weather_service.rb
@@ -0,0 +1,18 @@
+class WeatherService < GeneralService
+
+  def forecast(latitude, longitude)
+    params = {lat: latitude,
+              lon: longitude,
+              units: 'imperial',
+              exclude: 'minutely'}
+    x = get_json('/data/2.5/onecall', params)
+    binding.pry
+  end
+
+  private
+  def conn
+    Faraday.new(url: 'https://api.openweathermap.org') do |f|
+      f.params[:appid] = ENV['openweather_key']
+    end
+  end
+end
