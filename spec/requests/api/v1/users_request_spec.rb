64194c2986747d0cceb7b8ef0dd011c3493a902b
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-03 14:46:21 -0600

    add more sad path tests

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -34,5 +34,76 @@ RSpec.describe "Api::V1::Users", type: :request do
       expect(errors[:errors].count).to eq(1)
       expect(errors[:errors].first).to eq("Password confirmation doesn't match Password")
     end
+
+    it "will not create user if password is not present" do
+      request_params = {
+              email: "whatever@example.com",
+              }
+
+      post "/api/v1/users", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:bad_request)
+                      
+      errors = JSON.parse(response.body, symbolize_names: true)
+                        
+      expect(errors[:errors].count).to eq(1)
+      expect(errors[:errors].first).to eq("Password can't be blank")
+    end
+
+    it "will not create user if email is not present" do
+      request_params = {
+              password: "password",
+              password_confirmation: "password",
+              }
+
+      post "/api/v1/users", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:bad_request)
+                      
+      errors = JSON.parse(response.body, symbolize_names: true)
+      expect(errors[:errors].count).to eq(1)
+      expect(errors[:errors].first).to eq("Email can't be blank")
+    end
+
+    it "will not create a user if email is already taken" do
+      request_params = {
+              email: "whatever@example.com",
+              password: "password",
+              password_confirmation: "password"
+              }
+      user = User.create({
+              email: "whatever@example.com",
+              password: "password",
+              password_confirmation: "password"
+              })
+              
+      post "/api/v1/users", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:bad_request)
+                      
+      errors = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(errors[:errors].count).to eq(1)
+      expect(errors[:errors].first).to eq('Email has already been taken')
+    end
+
+    it "will return multiple errors if multple validations fail" do
+      request_params = {
+              email: "whatever@example.com",
+              password: "password1",
+              password_confirmation: "password"
+              }
+      user = User.create({
+              email: "whatever@example.com",
+              password: "password",
+              password_confirmation: "password"
+              })
+              
+      post "/api/v1/users", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:bad_request)
+                      
+      errors = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(errors[:errors].count).to eq(2)
+      expect(errors[:errors].first).to eq('Email has already been taken')
+      expect(errors[:errors].last).to eq("Password confirmation doesn't match Password")
+    end
   end
 end
