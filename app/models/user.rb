21029e6bf23d637cf6e529fd5045adaa54e3ea5a
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 20:52:45 -0600

    add user validations and api key assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true
+  validates_presence_of :password_confirmation, :if => :password_digest_changed?
+  validates :password, presence: true, confirmation: true
+  has_secure_password
+
+  before_create :assign_api_key
+
+  private
+  def assign_api_key
+    self.api_key = SecureRandom.uuid
+  end
 end
