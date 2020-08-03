c4710825d13b3410e69478cf743498c7a04ace06
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-02 20:52:20 -0600

    add user create method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user)
+    else
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
 
-  describe "posy /create" do
+  describe "post /create" do
     it "returns http success" do
       request_params = {
                         email: "whatever@example.com",
-                        password: "password"
+                        password: "password",
                         password_confirmation: "password"
                         }
-      post "/api/v1/users/create", params: request_params
+      post "/api/v1/users", params: request_params
       expect(response).to have_http_status(:success)
-
+                      
       user = JSON.parse(response.body, symbolize_names: true)
-
+                        
       expect(user[:data][:attributes][:email]).to eq('whatever@example.com')
       expect(user[:data][:attributes][:api_key]).to_not be_nil
     end
