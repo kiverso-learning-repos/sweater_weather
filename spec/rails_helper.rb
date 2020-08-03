2506b597ecaecd4773b6ffe3ad8f96fe37f45cb9
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-03 11:13:12 -0600

    filter sensitive data in VCRs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,4 +71,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
+  config.filter_sensitive_data('<openweather_api_key>') { ENV['openweather_key'] }
+  config.filter_sensitive_data('<unsplash_api_key>') { ENV['unsplash_key'] }
+  config.filter_sensitive_data('<hiking_project_key>') { ENV['hiking_project_key'] }
 end
