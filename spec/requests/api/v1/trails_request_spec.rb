7cc160e9e1c774892b9fee5bb19a26cda044af87
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-03 11:14:27 -0600

    update spec

--- spec/requests/api/v1/trails_request_spec.rb
+++ spec/requests/api/v1/trails_request_spec.rb
@@ -8,18 +8,17 @@ RSpec.describe "Api::V1::Trails", type: :request do
       get "/api/v1/trails", params: request_params
       expect(response).to have_http_status(:success)
 
-      trails = JSON.parse(response.body, symbolize_name: true)
+      trails = JSON.parse(response.body, symbolize_names: true)
 
       expect(trails[:data][:attributes][:location]).to eq("Denver, CO")
       expect(trails[:data][:attributes][:forecast][:summary]).to_not be_nil
-      expect(trails[:data][:attributes][:forecast][:temp]).to_not be_nil
+      expect(trails[:data][:attributes][:forecast][:temperature]).to_not be_nil
       trails[:data][:attributes][:trails].each do |trail|
         expect(trail[:name]).to_not be_nil
         expect(trail[:summary]).to_not be_nil
         expect(trail[:difficulty]).to_not be_nil
         expect(trail[:location]).to_not be_nil
         expect(trail[:distance_to_trail]).to_not be_nil
-        expect(trail[:estimated_travel_time]).to_not be_nil
       end
     end
   end
