cd0728918db402c8c00e77763425be54db959554
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-04 10:19:22 -0600

    Add tests for login

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -2,11 +2,72 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Sessions", type: :request do
 
-  describe "GET /create" do
+  describe "POST /sessions" do
+    before(:each) do
+      @user = User.create({
+              email: "whatever@example.com",
+              password: "password",
+              password_confirmation: "password"
+              })
+    end
     it "returns http success" do
-      get "/api/v1/sessions/create"
+      request_params = {
+              email: "whatever@example.com",
+              password: "password"
+              }
+
+      post "/api/v1/sessions", params: request_params.to_json, headers: { "Content-Type": "application/json" }
       expect(response).to have_http_status(:success)
+                      
+      session = JSON.parse(response.body, symbolize_names: true)
+                        
+      expect(session[:data][:attributes][:email]).to eq(@user.email)
+      expect(session[:data][:attributes][:api_key]).to eq(@user.api_key)
+      expect(session[:data][:attributes][:password]).to be_nil
+    end
+
+    it "returns an error if email is not correct" do
+      request_params = {
+              email: "whatever1@example.com",
+              password: "password"
+              }
+
+      post "/api/v1/sessions", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:success)
+                                              
+      errors = JSON.parse(response.body, symbolize_names: true)
+
+      expect(errors[:errors].count).to eq(1)
+      expect(errors[:errors].first).to eq("Invalid email or password")
+    end
+
+    it "returns an error if email is not present" do
+      request_params = {
+              password: "password"
+              }
+
+      post "/api/v1/sessions", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:success)
+                                              
+      errors = JSON.parse(response.body, symbolize_names: true)
+
+      expect(errors[:errors].count).to eq(1)
+      expect(errors[:errors].first).to eq("Invalid email or password")
+    end
+
+    it "returns an error if password is not correct" do
+      request_params = {
+              email: "whatever@example.com",
+              password: "wrong_password"
+              }
+
+      post "/api/v1/sessions", params: request_params.to_json, headers: { "Content-Type": "application/json" }
+      expect(response).to have_http_status(:success)
+                                              
+      errors = JSON.parse(response.body, symbolize_names: true)
+
+      expect(errors[:errors].count).to eq(1)
+      expect(errors[:errors].first).to eq("Invalid email or password")
     end
   end
-
 end
