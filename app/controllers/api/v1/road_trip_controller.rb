28a44ce573151ea3ac56673ef9efe8bb3db97ac5
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-04 13:44:01 -0600

    Add road_trip_serializer

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: road_trip_params[:api_key])
-    binding.pry
     if user
       if road_trip_params[:destination].present? && road_trip_params[:origin].present?
         road_trip = RoadTripFacade.new(road_trip_params[:origin], road_trip_params[:destination])
+        render json: RoadTripSerializer.new(road_trip)
       else render json: {errors: ['Origin and destination are required']}, status: :bad_request 
-      end 
+      end
     else
       render json: {errors: ['Invalid API key']}, status: :unauthorized
     end
