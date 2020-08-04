a64b18d867a8903caf97b0117fcf41f5f9ce83a0
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-04 13:27:59 -0600

    Add instance variables to road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,13 +1,24 @@
-class RoadTripFacade
+class RoadTripFacade < ForecastFacade
   attr_reader :id, :origin, :destination, :travel_time, :arrival_forecast
 
   def initialize(origin, destination)
     directions = get_directions(origin, destination)
+    forecast = get_forecast(directions.destination_location[:lat], directions.destination_location[:lng])
+    @id = 'null'
+    @origin = directions.origin
+    @destination = directions.destination
+    @destination = directions.travel_time
+    @arrival_forecast = format_forcast(forecast)
   end
 
   def get_directions(origin, destination)
     map_service = MapService.new
     location_data = map_service.directions(origin, destination)
     Direction.new(location_data)
+  end
+
+  def format_forecast(forecast)
+    {summary: forecast.current[:weather][0][:description],
+     temperature: forecast.current[:temp]}
   end
 end
