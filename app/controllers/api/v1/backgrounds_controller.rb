c73a99eeeec90c95736adfada9f20e4f0e07b2dd
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-04 20:21:26 -0600

    Add sad path for location

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    location_data = MapService.new.location(params[:location])
-    location = Location.new(location_data)
-    forecast_data = WeatherService.new.forecast(location.latitude, location.longitude)
-    forecast = Forecast.new(forecast_data)
-    image_data = ImageService.new.background(forecast.current[:weather][0][:description])
-    render json: BackgroundSerializer.new(Background.new(image_data[:results][0]))
+    if params[:location].present?
+      location_data = MapService.new.location(params[:location])
+      location = Location.new(location_data)
+      forecast_data = WeatherService.new.forecast(location.latitude, location.longitude)
+      forecast = Forecast.new(forecast_data)
+      image_data = ImageService.new.background(forecast.current[:weather][0][:description])
+      render json: BackgroundSerializer.new(Background.new(image_data[:results][0]))
+    else
+      render json: {errors: ['Location is required.']}, status: :bad_request
+    end
   end
 
 end
