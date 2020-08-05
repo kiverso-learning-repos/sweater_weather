20fb8f8df91b9fdfcb5df3bcfd2008286ab18adb
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-08-04 20:22:33 -0600

    Rescue for nomethod due to  API not finding data

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::RoadTripController < ApplicationController
     user = User.find_by(api_key: road_trip_params[:api_key])
     if user
       if road_trip_params[:destination].present? && road_trip_params[:origin].present?
+        begin
         road_trip = RoadTripFacade.new(road_trip_params[:origin], road_trip_params[:destination])
+        rescue NoMethodError
+          return render json: {errors: ["Looks like there was a problem finding these locations."]}, status: :bad_request 
+        end
         render json: RoadTripSerializer.new(road_trip)
       else render json: {errors: ['Origin and destination are required']}, status: :bad_request 
       end
